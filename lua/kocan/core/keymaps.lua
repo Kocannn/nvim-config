01e66acdc59e1239d1108cf967df2401366dad45
Kocannn <dwicandraandika4@gmail.com>
2025-03-05 01:10:15 +0800

    add diagnostic keymap

--- lua/kocan/core/keymaps.lua
+++ lua/kocan/core/keymaps.lua
@@ -30,3 +30,8 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- diagnostic
+keymap.set("n", "<C-j>", function()
+	vim.lsp.diagnostic.goto_next()
+end, { desc = "Go to next diagnostic" })
