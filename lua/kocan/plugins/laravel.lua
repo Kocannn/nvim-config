f864ffc0f8991686f457fd45fd7fb93217c25fbb
Kocannn <dwicandraandika4@gmail.com>
2025-03-05 01:12:27 +0800

    add laravel plugins

--- lua/kocan/plugins/laravel.lua
+++ lua/kocan/plugins/laravel.lua
@@ -1,13 +1,26 @@
 return {
-	"adalessa/laravel.nvim",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-telescope/telescope.nvim",
-		"nvim-lua/plenary.nvim",
-		"kevinhwang91/promise-async",
-		"MunifTanjim/nui.nvim",
+	{
+		"adalessa/laravel.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+			"kevinhwang91/promise-async",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("laravel").setup()
+		end,
 	},
-	config = function()
-		require("laravel").setup()
-	end,
+	{
+		"ricardoramirezr/blade-nav.nvim",
+		dependencies = { -- totally optional
+			"hrsh7th/nvim-cmp", -- if using nvim-cmp
+			{ "ms-jpq/coq_nvim", branch = "coq" }, -- if using coq
+		},
+		ft = { "blade", "php" }, -- optional, improves startup time
+		opts = {
+			close_tag_on_complete = true, -- default: true
+		},
+	},
 }
