7763473db44b743e3d05f8f17d56a0666869c705
Kocannn <dwicandraandika4@gmail.com>
2025-04-04 15:05:57 +0800

    hehe

--- init.lua
+++ init.lua
@@ -1,8 +1,1 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
-vim.filetype.add({
-  extension = {
-    edge = "html", -- Perlakukan .edge seperti HTML agar autotag bekerja
-  },
-})
+require("kocan")
--- lua/plugins/edge-template.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "watzon/vim-edge-template",
-    config = function()
-      -- Konfigurasi plugin di sini
-      print("vim-edge-template loaded!")
-    end,
-  },
-}
