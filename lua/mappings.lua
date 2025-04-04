b0d9690be03a8d48842ba738c1469f2b1e2f8c08
Kocannn <dwicandraandika4@gmail.com>
2025-04-04 14:57:14 +0800

    hehehe

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,8 +3,24 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local nomap = vim.keymap.del
+local opts = { noremap = true, silent = true }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+-- Window movement
+map("n", "sl", "<C-w>l", opts)
+map("n", "sh", "<C-w>h", opts)
+map("n", "sk", "<C-w>k", opts)
+map("n", "sj", "<C-w>j", opts)
+
+nomap("n", "<C-h>")
+nomap("n", "<C-l>")
+
+-- Diagnostics
+map("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -13,13 +13,31 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
+  {
+    "IogaMaster/neocord",
+    event = "VeryLazy",
+
+    config = function()
+      require "configs.discord"
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    config = function()
+      require "configs.copilot"
+    end,
+  },
 }
