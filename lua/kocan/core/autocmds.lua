f16a52d51f444e18249360f42d2e8fec7ff5990e
Kocannn <dwicandraandika4@gmail.com>
2025-04-05 16:25:37 +0800

    hehe

--- lua/kocan/core/autocmds.lua
+++ lua/kocan/core/autocmds.lua
@@ -25,3 +25,14 @@ autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
 		end
 	end,
 })
+
+autocmd("TermOpen", {
+	pattern = "*",
+	callback = function()
+		local term_title = vim.b.term_title
+		if term_title and term_title:match("lazygit") then
+			-- Create lazygit specific mappings
+			vim.keymap.set("t", "q", "<cmd>close<cr>", { buffer = true })
+		end
+	end,
+})
