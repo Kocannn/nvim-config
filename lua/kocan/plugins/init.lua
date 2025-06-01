ac761576fa87e78038f1fbaf372901d37e4ea01e
Kocannn <dwicandraandika4@gmail.com>
2025-06-01 16:59:17 +0800

    feat(snacks): add picker/input config and ui-select support
    - Added detailed configuration for snacks.nvim input window, including
      position, border, title, size, and key mappings.
    - Enabled picker integration with telescope-ui-select.nvim.
    - Updated question header for copilot-chat.nvim to include a custom
    prefix.
    - Updated lazy-lock.json to add telescope-ui-select.nvim and remove
    showkeys.

--- lua/kocan/plugins/init.lua
+++ lua/kocan/plugins/init.lua
@@ -277,7 +277,7 @@ return {
 			user = user:sub(1, 1):upper() .. user:sub(2)
 			return {
 				auto_insert_mode = true,
-				question_header = "  " .. user .. " ",
+				question_header = " Yang Mulia Maha Raja " .. user .. " ",
 				answer_header = "  Copilot ",
 				window = {
 					width = 0.4,
@@ -352,9 +352,56 @@ return {
 	},
 	{
 		"folke/snacks.nvim",
+		---@type snacks.Config
+		---@class snacks.input.Config
+		---@field enabled? boolean
+		---@field win? snacks.win.Config|{}
+		---@field icon? string
+		---@field icon_pos? snacks.input.Pos
+		---@field prompt_pos? snacks.input.Pos
 		priority = 1000,
 		lazy = false,
-		opts = {},
+		opts = {
+			picker = {
+				ui_select = { enable = true },
+			},
+			input = {
+				backdrop = false,
+				position = "float",
+				border = "rounded",
+				title_pos = "center",
+				height = 1,
+				width = 60,
+				relative = "editor",
+				noautocmd = true,
+				row = 2,
+				-- relative = "cursor",
+				-- row = -3,
+				-- col = 0,
+				wo = {
+					winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
+					cursorline = false,
+				},
+				bo = {
+					filetype = "snacks_input",
+					buftype = "prompt",
+				},
+				--- buffer local variables
+				b = {
+					completion = false, -- disable blink completions in input
+				},
+				keys = {
+					n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n", expr = true },
+					i_esc = { "<esc>", { "cmp_close", "stopinsert" }, mode = "i", expr = true },
+					i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = { "i", "n" }, expr = true },
+					i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i", expr = true },
+					i_ctrl_w = { "<c-w>", "<c-s-w>", mode = "i", expr = true },
+					i_up = { "<up>", { "hist_up" }, mode = { "i", "n" } },
+					i_down = { "<down>", { "hist_down" }, mode = { "i", "n" } },
+					q = "cancel",
+				},
+			},
+		},
 		config = function(_, opts)
 			require("snacks").setup(opts)
 			-- HACK: restore vim.notify after snacks setup and let noice.nvim take over
